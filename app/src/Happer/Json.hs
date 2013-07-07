c8c44c9743fea4a73bcd5749773800a5ab158596
Xavier Shay <xavier@rhnh.net>
2013-07-06 19:12:25 -0700

    JSON serializers.

---
+++ app/src/Happer/Json.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- JSON converters for all basic Happer types.
+module Happer.Json where
+
+import Happer.Types
+
+import Control.Applicative ( (<$>), (<*>), pure )
+
+ -- a JSON library
+import Data.Aeson
+import Data.Attoparsec.Number ( Number(I) )
+
+instance FromJSON FreeSpan where
+  parseJSON (Object v) = FreeSpan                        <$>
+                         v .: "traceId"                  <*>
+                         v .:? "parentId" .!= (SpanId 0) <*>
+                         v .: "span"
+  parseJSON _          = fail "Unknown error parsing new span"
+
+instance FromJSON SpanId where
+  parseJSON (Number (I x)) = pure $ SpanId x
+  parseJSON _              = fail "Span ID is not an integer"
+
+instance ToJSON SpanId where
+  toJSON (SpanId x) = Number (I x)
+
+instance FromJSON Span where
+  parseJSON (Object v) = Span                    <$>
+                         v .: "spanId"           <*>
+                         v .: "name"             <*>
+                         v .: "startTime"        <*>
+                         v .: "endTime"          <*>
+                         v .:? "children" .!= []
+
+instance ToJSON Span where
+  toJSON s = object [ "spanId"    .= (spanId s)
+                    , "name"      .= (name s)
+                    , "startTime" .= (startTime s)
+                    , "endTime"   .= (startTime s)
+                    , "children"  .= (children s)
+                    ]
--- app/src/Main.hs
+++ app/src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Happstack.Server
 import Happer.Types
 import Happer.Persistence
+import Happer.Json
 
 main :: IO ()
 main = simpleHTTP nullConf $ ok "Hello, World!"
